1208c1ad09cf10316708c1722ea208ee0616e32e
maksiaev <alexander.maksiaev@gmail.com>
2025-06-27 21:19:16 -0400

    Need to re-work code because 500+ minutes is way too much

--- bash/D1.1_iqtree.sh
+++ bash/D1.1_iqtree.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
-#SBATCH --job-name="A3 iqtree"
+#SBATCH --job-name="all D1.1 iqtree"
 #SBATCH --mail-type=BEGIN,END,FAIL
-#SBATCH --mail-user="alexander.maksiaev@nih.gov"
+#SBATCH --mail-user="alvin.crespobellido@nih.gov"
 ##################################################
 ######################
 
 module load iqtree
-srun  iqtree2 -s A3_full_genome_11-01-2021--06-13-2025_aln.fas -m GTR+G -bb 1000 -nt AUTO
+srun  iqtree2 -s newid-D1.1_concat_genome_MAY14_trim_aln_n2240_FINAL.fasta -m GTR+G -bb 1000 -nt AUTO
 
 ######################
 date +"%Y-%m-%d %H:%M"
