c6356ce01f2ccd9823bd7633ff125fce78534e48
maksiaev <alexander.maksiaev@gmail.com>
2025-07-03 10:26:24 -0400

    Concatenator script begin and writing better comments

--- bash/D1.1_iqtree.sh
+++ bash/D1.1_iqtree.sh
@@ -6,7 +6,7 @@
 ######################
 
 module load iqtree
-srun  iqtree2 -s newid-D1.1_concat_genome_MAY14_trim_aln_n2240_FINAL.fasta -m GTR+G -bb 1000 -nt AUTO
+iqtree2 -s newid-D1.1_concat_genome_MAY14_trim_aln_n2240_FINAL.fasta -m GTR+G -bb 1000 -nt AUTO
 
 ######################
 date +"%Y-%m-%d %H:%M"
