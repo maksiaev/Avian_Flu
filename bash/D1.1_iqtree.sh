f15d79d91d5f5f03c60817db84901ef021c31593
maksiaev <alexander.maksiaev@gmail.com>
2025-08-13 11:33:08 -0400

    Making trees

--- bash/D1.1_iqtree.sh
+++ bash/D1.1_iqtree.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
-#SBATCH --job-name="all A3 iqtree"
+#SBATCH --job-name="all B3.13 iqtree"
 #SBATCH --mail-type=BEGIN,END,FAIL
 #SBATCH --mail-user="alexander.maksiaev@nih.gov"
 ##################################################
 ######################
 
 module load iqtree
-iqtree2 -s A3_11-01-2021--07-25-2025_concat_330.fasta -m GTR+G -bb 1000 -nt AUTO
+iqtree2 -s B3.13_11-01-2021--08-08-2025_concat_4729.fasta -m GTR+G -bb 1000 -nt AUTO
 
 ######################
 date +"%Y-%m-%d %H:%M"
