a406af6436efda35b49b0695614927e2302fa689
maksiaev <alexander.maksiaev@gmail.com>
2025-12-05 13:20:09 -0500

    B3.13 alignments and rejections

--- bash/iqtree.sh
+++ bash/iqtree.sh
@@ -6,7 +6,7 @@
 ######################
 
 module load iqtree
-iqtree2 -s D1.3_concat_321.fasta -m GTR+G -bb 1000 -nt AUTO
+iqtree2 -s B3.13_concat_4997.fasta -m GTR+G -bb 1000 -nt AUTO
 
 ######################
 date +"%Y-%m-%d %H:%M"
