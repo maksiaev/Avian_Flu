aa5ee653bb34af519853d4b56c8d13543b15667c
maksiaev <alexander.maksiaev@gmail.com>
2025-12-05 15:37:25 -0500

    B3.13, D1.1, and D1.3 trees 11 28 2025

---
+++ bash/D1.1_iqtree.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+#SBATCH --job-name="all D1.1 iqtree"
+#SBATCH --mail-type=BEGIN,END,FAIL
+#SBATCH --mail-user="alexander.maksiaev@nih.gov"
+##################################################
+######################
+
+module load iqtree
+iqtree2 -s D1.1_concat_2866.fasta -m GTR+G -bb 1000 -nt AUTO
+
+######################
+date +"%Y-%m-%d %H:%M"
---
+++ bash/D1.3_iqtree.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+#SBATCH --job-name="all D1.3 iqtree"
+#SBATCH --mail-type=BEGIN,END,FAIL
+#SBATCH --mail-user="alexander.maksiaev@nih.gov"
+##################################################
+######################
+
+module load iqtree
+iqtree2 -s D1.3_concat_266.fasta -m GTR+G -bb 1000 -nt AUTO
+
+######################
+date +"%Y-%m-%d %H:%M"
