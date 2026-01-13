24886f70dfb07f357ffd86bd1ed9fca3086c13a8
maksiaev <alexander.maksiaev@gmail.com>
2026-01-13 11:40:35 -0500

    Updating 6/13/2025 dataset (all genotypes) after finding discrepancies with GISAID sequences in D1.1 pre-print

--- utils.py
+++ utils.py
@@ -643,9 +643,9 @@ def open_gisaid(username, password, browser, sleep_time, continent, start_date, 
     # Find the ID for N
 
     # N: 1
-    # n_1 = driver.find_element(By.XPATH, "//*[contains(@class, 'sys-event-hook sys-fi-mark')]//option[@value='2.3.4.4b']") # The third multi-select
-    # driver.execute_script("arguments[0].scrollIntoView();", n_1)
-    # ActionChains(driver).move_to_element(n_1).pause(1).click(n_1).perform() 
+    n_1 = driver.find_element(By.XPATH, "//*[contains(@class, 'sys-event-hook sys-fi-mark')]//option[@value='2.3.4.4b']") # The third multi-select
+    driver.execute_script("arguments[0].scrollIntoView();", n_1)
+    ActionChains(driver).move_to_element(n_1).pause(1).click(n_1).perform() 
 
     time.sleep(sleep_time)
     
