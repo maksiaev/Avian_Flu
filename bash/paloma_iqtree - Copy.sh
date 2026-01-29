6c437374a19f5ad5f9ff171d01bf88151cd5b5f8
maksiaev <alexander.maksiaev@gmail.com>
2026-01-29 11:57:50 -0500

    Running Paloma trees and updating 1/23/2026 dataset

---
+++ bash/paloma_iqtree - Copy.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+#SBATCH --job-name="Paloma iqtree"
+#SBATCH --mail-type=BEGIN,END,FAIL
+#SBATCH --mail-user="alexander.maksiaev@nih.gov"
+##################################################
+######################
+
+module load iqtree
+iqtree2 -s HA_H1_Paloma_aln_trimmed_deduplicated.fasta -m GTR+G -bb 1000 -nt AUTO
+
+######################
+date +"%Y-%m-%d %H:%M"
