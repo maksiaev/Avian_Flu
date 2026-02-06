e28822a2cfb47518d6de95600cacb7be7fef320e
maksiaev <alexander.maksiaev@gmail.com>
2026-02-06 16:07:46 -0500

    Paloma trees continue

--- bash/paloma_iqtree - Copy (9).sh
+++ bash/paloma_iqtree - Copy (9).sh
@@ -1,12 +1,12 @@
 #!/bin/bash
-#SBATCH --job-name="Paloma iqtree"
+#SBATCH --job-name="PB1 Paloma iqtree"
 #SBATCH --mail-type=BEGIN,END,FAIL
 #SBATCH --mail-user="alexander.maksiaev@nih.gov"
 ##################################################
 ######################
 
 module load iqtree
-iqtree2 -s PB1_Paloma_aln_trimmed.fasta -m GTR+G -bb 1000 -nt AUTO
+iqtree2 -s PB1_Paloma_aln_trimmed_deduplicated.fasta -m GTR+G -bb 1000 -nt AUTO
 
 ######################
 date +"%Y-%m-%d %H:%M"
