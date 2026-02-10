43048a2eb7746c71c095919705c7d251db8f26bd
maksiaev <alexander.maksiaev@gmail.com>
2026-02-10 11:20:07 -0500

    PARNAS for Paloma

---
+++ bash/parnas_PA.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+#SBATCH --job-name="PA parnas"
+#SBATCH --mail-type=BEGIN,END,FAIL
+#SBATCH --mail-user="alexander.maksiaev@nih.gov"
+##################################################
+######################
+
+source myconda
+mamba activate base
+
+conda activate parnas_env
+
+parnas -t PA_Paloma_aln_trimmed_deduplicated.fasta.treefile -n 100 --exclude-fully ".*human.*|.*\\|Iberian|.*\\|White" --subtree "PA_Paloma_aln_trimmed_deduplicated_subsampled.treefile"
+
+######################
+date +"%Y-%m-%d %H:%M"
