aec483e8c46a98f0a82d7d6972585e507ec7e35c
maksiaev <alexander.maksiaev@gmail.com>
2026-02-19 16:54:00 -0500

    Making Paloma trees and creating metadata maker

--- bash/Paloma/paloma_iqtree - Copy (5).sh
+++ bash/Paloma/paloma_iqtree - Copy (5).sh
@@ -6,7 +6,7 @@
 ######################
 
 module load iqtree
-iqtree2 -s NA_N2_Paloma_aln_trimmed_deduplicated.fasta -m GTR+G -bb 1000 -nt AUTO
+iqtree2 -s NA_N2_Paloma_aln_trim.fasta -m GTR+G -bb 1000 -nt AUTO
 
 ######################
 date +"%Y-%m-%d %H:%M"
