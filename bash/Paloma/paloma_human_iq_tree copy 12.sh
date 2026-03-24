df1382f7f66de4e30a1b257c21372571c857422c
maksiaev <alexander.maksiaev@gmail.com>
2026-03-24 11:28:38 -0400

    Paloma IQ trees

---
+++ bash/Paloma/paloma_human_iq_tree copy 12.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+#SBATCH --job-name="HA H3 Paloma iqtree"
+#SBATCH --mail-type=BEGIN,END,FAIL
+#SBATCH --mail-user="alexander.maksiaev@nih.gov"
+##################################################
+######################
+
+module load iqtree
+iqtree2 -s PB2_Paloma_wLineage_wDatesAdded_v7_aln_trim.fasta -m GTR+G -bb 1000 -nt AUTO
+
+
+######################
+date +"%Y-%m-%d %H:%M"
